ddfe5f2e83234410a5236b9d5315fb4aba6f590b
Player Ghost <playerghost13@gmail.com>
2020-09-23 22:58:36 -0300

    b

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { AjudaComponent } from './page/ajuda/ajuda.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
-  { path: "home", component: HomeComponent },
-  { path: "ajuda", component: AjudaComponent }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+},
+  {path: "home", component: HomeComponent},
+  { path: "ajuda", component: AjudaComponent}
 ];
 
 @NgModule({
--- src/app/page/ajuda/ajuda.component.ts
+++ src/app/page/ajuda/ajuda.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajuda.component.css']
 })
 export class AjudaComponent implements OnInit {
+  acc: HTMLCollection
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.acc = document.getElementsByClassName("accordion");
   }
 
+  ngOnInit(): void {
+
+    for (let i = 0; i < this.acc.length; i++) {
+      this.acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }
 }
