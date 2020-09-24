84e6c4e7c54be79ce10e617e8b17eed4001c1065
Player Ghost <playerghost13@gmail.com>
2020-09-23 22:58:36 -0300

    b

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,11 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
+
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    let date = new Date(Date.now());
 
+    document.getElementById("data").textContent = date.toLocaleDateString('PT-BR')
+  }
 }
