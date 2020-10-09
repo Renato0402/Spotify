95f7518ee1cb4965970694a83b2ce44c027b760b
Player Ghost <playerghost13@gmail.com>
2020-10-09 16:58:13 -0300

    a

--- src/app/mock/playlistsMock.ts
+++ src/app/mock/playlistsMock.ts
@@ -14,7 +14,7 @@ export class PlaylistsMock {
         this.playlists[4] = {id: 4, nome: "The Day Is my Enemy", musicas: [], capa: "assets/images/capas/TheDayIsMyEnemy.jpg"}
         this.playlists[5] = {id: 5, nome: "Trap-Rap", musicas: [], capa: "assets/images/capas/Trap-Rap.jpg"}
     
-        this.playlists[0].musicas[0] = {id: 0, nome: "24kGoldn - Mood (feat. iann dior)", album: "", artista: "24kGoldn", duration: 132600, audio: "assets/musics/Playlists/As 50 mais tocadas no mundo/24kGoldn - Mood (feat. iann dior).mp3"}
+        this.playlists[0].musicas[0] = {id: 0, nome: "24kGoldn - Mood (feat. iann dior)", album: "Unknown", artista: "24kGoldn", duration: 132600, audio: "assets/musics/Playlists/As 50 mais tocadas no mundo/24kGoldn - Mood (feat. iann dior).mp3"}
         this.playlists[0].musicas[1] = {id: 1, nome: "BTS - Dynamite", album: "Dynamite (NightTime Version)", artista: "BTS", duration: 190200, audio: "assets/musics/Playlists/As 50 mais tocadas no mundo/BTS - Dynamite.mp3"}
         this.playlists[0].musicas[2] = {id: 2, nome: "Internet Money - Lemonade (feat. Gunna, Don Toliver & NAV)", album: "B4 The Storm", artista: "Internet Money", duration: 189600, audio: "assets/musics/Playlists/As 50 mais tocadas no mundo/Internet Money - Lemonade (feat. Gunna, Don Toliver & NAV).mp3"}
         this.playlists[0].musicas[3] = {id: 3, nome: "Justin Bieber - Holy (feat. Chance the Rapper)", album: "Holy", artista: "Justin Bieber", duration: 199200, audio: "assets/musics/Playlists/As 50 mais tocadas no mundo/Justin Bieber - Holy (feat. Chance the Rapper).mp3"}
