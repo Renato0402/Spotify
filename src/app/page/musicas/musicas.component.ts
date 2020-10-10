064ebe51fb171a931b20ac927f5ba4ebb7b2766a
Player Ghost <playerghost13@gmail.com>
2020-10-09 23:24:13 -0300

    a

--- src/app/page/musicas/musicas.component.ts
+++ src/app/page/musicas/musicas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaylistsMock } from 'src/app/mock/playlistsMock';
 import { PlaylistClicadaService } from 'src/app/services/playlist-clicada.service';
+import ColorThief from 'colorthief/dist/color-thief.min.js';
 
 @Component({
   selector: 'app-musicas',
@@ -20,10 +21,28 @@ export class MusicasComponent implements OnInit {
 
   }
 
+  coverBackground(): any {
+    let img = <HTMLImageElement>document.getElementById("coverBackground")
+
+    img.onload = function () {
+      let colorThief = new ColorThief();
+      let color = colorThief.getColor(img)
+      let RGBColors = String(color).replace(",", "").replace(",", "")
+      let HEXColors = Number(RGBColors).toString(16)
+
+      if(HEXColors.length < 2){
+        HEXColors = "0" + HEXColors
+      }
+
+      var a = {'background-color': "#" + HEXColors.toString}
+
+      return a;
+    };
+  }
+
   castIndex(index: Number){
     let aux = index
 
     return Number(aux)
   }
-
 }
