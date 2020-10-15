3ac87b11e5f927962769aea40a0fe644274ea306
Player Ghost <playerghost13@gmail.com>
2020-10-15 08:46:29 -0300

    a

--- src/app/page/musicas/musicas.component.ts
+++ src/app/page/musicas/musicas.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PlaylistsMock } from 'src/app/mock/playlistsMock';
 import { PlaylistClicadaService } from 'src/app/services/playlist-clicada.service';
 import ColorThief from 'colorthief/dist/color-thief.min.js';
 import { Musica } from 'src/app/entidades/musica';
+import { Router, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-musicas',
@@ -16,8 +17,8 @@ export class MusicasComponent implements OnInit {
   audio: HTMLAudioElement
   require: any
   lastMusicId: number
-  
-  constructor(service: PlaylistClicadaService) {
+
+  constructor(service: PlaylistClicadaService, private router: Router) {
     this.mock = new PlaylistsMock
     this.service = service
     this.isPlayingMusic = false
@@ -25,9 +26,27 @@ export class MusicasComponent implements OnInit {
 
   ngOnInit(): void {
     this.coverBackground()
+
+    this.router.events.subscribe((event) => {
+
+      if (event instanceof NavigationStart) {
+        //console.log("entrou")
+      }
+
+      if (event instanceof NavigationEnd) {
+        //playPause(this.lastMusicId)
+      }
+
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+
+        // Present error to user
+        console.log(event.error);
+      }
+    });
   }
 
-  coverBackground(){
+  coverBackground() {
     let img = <HTMLImageElement>document.getElementById("coverBackground")
 
     img.onload = function () {
@@ -38,11 +57,11 @@ export class MusicasComponent implements OnInit {
     };
   }
 
-  playPause(id: number){
-    let button = <HTMLImageElement>document.getElementById("button"+id)
+  playPause(id: number) {
+    let button = <HTMLImageElement>document.getElementById("button" + id)
 
-    if(this.isPlayingMusic && button.currentSrc == "http://localhost:4200/assets/images/icons/pause-button.png"){
-      let lastButton = <HTMLImageElement>document.getElementById("button"+ this.lastMusicId)
+    if (this.isPlayingMusic && button.currentSrc == "http://localhost:4200/assets/images/icons/pause-button.png") {
+      let lastButton = <HTMLImageElement>document.getElementById("button" + this.lastMusicId)
       lastButton.src = "assets/images/icons/play-button.png"
 
       this.audio.pause()
@@ -50,14 +69,14 @@ export class MusicasComponent implements OnInit {
       this.isPlayingMusic = false
 
       button.src = "assets/images/icons/play-button.png"
-    }else{
-      if(this.audio != undefined){
+    } else {
+      if (this.audio != undefined) {
         this.audio.pause()
 
-        let lastButton = <HTMLImageElement>document.getElementById("button"+ this.lastMusicId)
+        let lastButton = <HTMLImageElement>document.getElementById("button" + this.lastMusicId)
         lastButton.src = "assets/images/icons/play-button.png"
       }
-      
+
       this.audio = new Audio(this.mock.playlists[this.service.index].musicas[id].audio);
       this.audio.play()
       this.audio.volume = 0.5
@@ -70,13 +89,13 @@ export class MusicasComponent implements OnInit {
     }
   }
 
-  formatDuration(duration: number){
+  formatDuration(duration: number) {
     let formattedDuration = String(Math.round((duration / 60000) * 100) / 100).split(".")
-    
-    if(formattedDuration[1].length < 2){
-      formattedDuration[1] =  formattedDuration[1] + "0"
+
+    if (formattedDuration[1].length < 2) {
+      formattedDuration[1] = formattedDuration[1] + "0"
     }
 
-    return String(formattedDuration).replace(",",":")
+    return String(formattedDuration).replace(",", ":")
   }
 }
